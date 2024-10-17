84984f2d5389b2adf71fefe048f5e2c881655cb7
Waleed Ahmad <waleedwaris1234@gmail.com>
2024-10-17 16:08:01 +0500

    Imroved workspace item in main

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,7 +11,7 @@ android {
     }
     defaultConfig {
         applicationId = "com.example.teamscollaboration"
-        minSdk = 24
+        minSdk = 34
         targetSdk = 34
         versionCode = 1
         versionName = "1.0"
